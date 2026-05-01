fa77fc096c4f6b0a9622d67006137463409ce3f5
Ryan Tarnowski <ryan.tarnowski@gmail.com>
2026-05-01 07:59:18 -0600

    add JWT functions and rename password.go to auth.go

---
+++ internal/auth/auth.go
@@ -0,0 +1,80 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type TokenType string
+
+const (
+	TokenTypeAccess TokenType = "chirpy-access"
+)
+
+func HashPassword(password string) (string, error) {
+	hashed_pw, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+	if err != nil {
+		return "", err
+	}
+
+	return hashed_pw, nil
+}
+
+func CheckPasswordHash(password, hash string) (bool, error) {
+	match, err := argon2id.ComparePasswordAndHash(password, hash)
+	if err != nil {
+		return false, err
+	}
+
+	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    string(TokenTypeAccess),
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	signingKey := []byte(tokenSecret)
+
+	signed_token, err := token.SignedString(signingKey)
+	if err != nil {
+		return "", err
+	}
+
+	return signed_token, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string(TokenTypeAccess) {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+
+	userID, err := uuid.Parse(subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userID, nil
+}
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,97 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestPasswordHashing(t *testing.T) {
+	cases := []struct {
+		input    string
+		input2   string
+		expected bool
+	}{
+		{
+			input:    "TestPassWord1",
+			input2:   "TestPassWord1",
+			expected: true,
+		},
+		{
+			input:    "TestPassWord1",
+			input2:   "WrongPassWord1",
+			expected: false,
+		},
+	}
+
+	for _, c := range cases {
+		hash, err := HashPassword(c.input)
+		if err != nil {
+			t.Errorf("Failed during password hashing %v", err)
+		}
+
+		result, err := CheckPasswordHash(c.input2, hash)
+		if err != nil {
+			t.Errorf("Failed during password compare %v", err)
+		}
+
+		if result != c.expected {
+			t.Errorf("Actual and expected do not match: \n'%v' \nvs\n'%v'", result, c.expected)
+		}
+	}
+}
+
+func TestJWTs(t *testing.T) {
+	userId, _ := uuid.Parse("84ad67f7-4da1-4fcc-a91e-68af9261d45a")
+
+	cases := []struct {
+		userId          uuid.UUID
+		secret          string
+		validate_secret string
+		expires_in      time.Duration
+		expected        bool
+	}{
+		{
+			userId:          userId,
+			secret:          "mySecret",
+			validate_secret: "mySecret",
+			expires_in:      2 * time.Minute,
+			expected:        true,
+		},
+		{
+			userId:          userId,
+			secret:          "mySecret",
+			validate_secret: "mySecret",
+			expires_in:      1,
+			expected:        false,
+		},
+		{
+			userId:          userId,
+			secret:          "mySecret",
+			validate_secret: "wrongSecret",
+			expires_in:      2 * time.Minute,
+			expected:        false,
+		},
+	}
+
+	for _, c := range cases {
+		token, err := MakeJWT(c.userId, c.secret, c.expires_in)
+		if err != nil {
+			t.Errorf("Failed to make JWT: %v", err)
+			continue
+		}
+
+		token_userId, err := ValidateJWT(token, c.validate_secret)
+		if err != nil {
+			if c.expected == true {
+				t.Errorf("Failed to validate JWT: %v", err)
+				continue
+			}
+		}
+
+		if c.userId != token_userId && c.expected == true {
+			t.Errorf("Actual and expected userIDs do not match: \n%v \vvs\n %v", c.userId, token_userId)
+		}
+	}
+}
